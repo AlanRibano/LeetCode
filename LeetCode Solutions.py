7e643600fcb95ee35861dfec238468391b40711e
AlanRibano <117466412+AlanRibano@users.noreply.github.com>
2022-11-28 17:55:04 -0700

    - completed plusOne and permute

--- LeetCode Solutions.py
+++ LeetCode Solutions.py
@@ -1,6 +1,6 @@
+from typing import List
 
-
-class Solution:
+class Solution():
     def reverseVowels(self, s: str) -> str:
         '''Given a string s, reverse only all the vowels in the string 
         and return it. The vowels are 'a', 'e', 'i', 'o', and 'u', and
@@ -16,7 +16,7 @@ class Solution:
             word[reversed_index[i[0]]] = i[1][1]
         return ''.join(word)    
 
-    def letterCombinations(self, digits: str) -> list[str]:
+    def letterCombinations(self, digits: str) -> List[str]:
         '''Given a string containing digits from 2-9 inclusive, return all 
         possible letter combinations that the number could represent. Return 
         the answer in any order.'''
@@ -32,7 +32,7 @@ class Solution:
         '''Implement pow(x, n), which calculates x raised to the power n (i.e., xn).'''
         return x**n
 
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         '''Given two sorted arrays nums1 and nums2 of size m and n respectively,
         return the median of the two sorted arrays.'''
 
@@ -44,5 +44,18 @@ class Solution:
         else:
             return nums1[len(nums1)//2]
 
+    def plusOne(self, digits: List[int]) -> List[int]:
+        '''Given a non-empty array of decimal digits representing a non-negative integer, 
+        increment one to the integer. The digits are stored such that the most significant 
+        digit is at the head of the list, and each element in the array contains a single 
+        digit. You may assume the integer does not contain any leading zero, except the 
+        number 0 itself.'''
+        return [int(i) for i in str(int(''.join([str(i) for i in digits])) + 1)]
 
-
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        '''Given an array nums of distinct integers, return all the possible permutations. 
+        You can return the answer in any order.'''
+        if len(nums) == 1:
+            return [nums]
+        else:
+            return [[i] + j for i in nums for j in self.permute([x for x in nums if x != i])]
